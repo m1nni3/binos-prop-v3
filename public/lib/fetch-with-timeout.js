47184dfe99c87b2dc7533d5859f99e11ba47658b
Mignon Stephane Dawson <3441356+m1nni3@users.noreply.github.com>
2026-07-06 15:17:53 +0200

    Add fetchWithTimeout helper

---
+++ public/lib/fetch-with-timeout.js
@@ -0,0 +1,10 @@
+export async function fetchWithTimeout(url, opts = {}, timeout = 15000) {
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), timeout);
+  try {
+    const res = await fetch(url, { ...opts, signal: controller.signal });
+    return res;
+  } finally {
+    clearTimeout(id);
+  }
+}
